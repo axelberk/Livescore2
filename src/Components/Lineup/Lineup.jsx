0e192e0ea73487f6fbcf9b277876ae2be8742546
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2025-05-05 16:48:14 +0200

    reversed lineups

--- src/Components/Lineup/Lineup.jsx
+++ src/Components/Lineup/Lineup.jsx
@@ -1,28 +1,47 @@
-import "./Lineup.css"
+import "./Lineup.css";
 
-const Lineup = ({team, color}) => {
-    const formationRows = team.formation.split("-").map(Number)
-    const players = team.startXI.map(p => p.player)
+const Lineup = ({ team, color, isAway }) => {
+  if (!team || !team.formation || !team.startXI) return null;
 
-    let playerIndex = 0
+  const formationRows = team.formation.split("-").map(Number);
+  const goalkeeper = team.startXI[0].player;
+  const outfieldPlayers = team.startXI.slice(1).map(p => p.player);
 
+  const players = isAway ? [...outfieldPlayers].reverse() : outfieldPlayers;
+  let playerIndex = 0;
+
+  const renderRow = (numPlayers, i) => {
+    const row = players.slice(playerIndex, playerIndex + numPlayers);
+    playerIndex += numPlayers;
     return (
-        <div className="pitch" style={{borderColor: color}}>
-            <div className="goalkeeper">
-                <div className="player">{players[playerIndex++].name}</div>
-            </div>
+      <div key={i} className="formation-row">
+        {row.map((p, j) => (
+          <div key={j} className="player">{p.name}</div>
+        ))}
+      </div>
+    );
+  };
 
-            {formationRows.map((numPlayers, i) => (
-                <div key={i} className="formation-row">
-                    {Array.from({length: numPlayers}).map((_, j) => (
-                        <div key={j} className="player">
-                            {players[playerIndex] ? players[playerIndex++].name : "-"}
-                        </div>
-                    ))}
-                </div>
-            ))}
+  const orderedRows = isAway ? [...formationRows].reverse() : formationRows;
+
+  return (
+    <div className="pitch" style={{ borderColor: color }}>
+      
+      {!isAway && (
+        <div className="goalkeeper">
+          <div className="player">{goalkeeper.name}</div>
         </div>
-    )
-}
+      )}
 
-export default Lineup
+      {orderedRows.map((num, i) => renderRow(num, i))}
+
+      {isAway && (
+        <div className="goalkeeper">
+          <div className="player">{goalkeeper.name}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Lineup;
